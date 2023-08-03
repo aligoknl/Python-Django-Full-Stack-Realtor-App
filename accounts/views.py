c4a21498275d99b86dd77c3c07f800ced091e901
Ali GÖK <97063504+aligoknl@users.noreply.github.com>
2023-08-03 21:07:14 +0200

    created register and login pages

---
+++ accounts/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+def register(request):
+    if request.method == 'POST':
+        messages.error(request, 'Testing error message')
+        return redirect('register')
+    return render(request, 'accounts/register.html')
+
+def login(request):
+    if request.method == 'POST':
+        return
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    return redirect(request, 'index')
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
--- btre/settings.py
+++ btre/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.humanize'
 ]
 
@@ -138,3 +139,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Messages
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
