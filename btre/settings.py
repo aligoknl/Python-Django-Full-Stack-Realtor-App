87c8eb018dc5e0427ae504334ef50ad5239a48dd
Ali GÖK <97063504+aligoknl@users.noreply.github.com>
2023-08-11 21:13:52 +0200

    created contact page

--- btre/settings.py
+++ btre/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.humanize'
 ]
 
